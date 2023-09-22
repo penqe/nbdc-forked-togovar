400a29c7648aca35644b600a5e82024b0cb2fa79
Yukiko Noda <yukiko8318@gmail.com>
2023-09-22 10:46:45 +0900

    add jsdoc to SearchButton class

--- app/frontend/javascripts/components/Common/SearchField/SimpleSearchButton.js
+++ app/frontend/javascripts/components/Common/SearchField/SimpleSearchButton.js
@@ -2,11 +2,16 @@ import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import Styles from '../../../../stylesheets/object/component/simple-search-button.scss';
 
+/**
+ * Class to create a simple search button
+ */
 @customElement('search-button')
-export default class SearchButton extends LitElement {
+class SearchButton extends LitElement {
   static styles = [Styles];
 
   render() {
     return html`<button class="btn" />`;
   }
 }
+
+export default SearchButton;
