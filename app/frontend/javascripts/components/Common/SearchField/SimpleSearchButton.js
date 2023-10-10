283ddbcd0ff5229deadf8c6cbfc5215c5acd02bc
Yukiko Noda <yukiko8318@gmail.com>
2023-10-10 19:15:05 +0900

    small fix

--- app/frontend/javascripts/components/Common/SearchField/SimpleSearchButton.js
+++ app/frontend/javascripts/components/Common/SearchField/SimpleSearchButton.js
@@ -2,9 +2,7 @@ import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import Styles from '../../../../stylesheets/object/component/simple-search-button.scss';
 
-/**
- * Class to create a simple search button
- */
+/** Class to create a simple search button */
 @customElement('search-button')
 class SearchButton extends LitElement {
   static styles = [Styles];
